2f1d30450b6e0c629ded6f0c498d2139960e0104
mirek079 <miroslawkowalski@079.gmail.com>
2019-02-13 14:39:36 +0100

    Pogoda szablon bootstrap

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,33 +16,10 @@ namespace Pogoda.Controllers
        public ViewResult Index() {
             
             int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            ViewBag.Greeting = hour < 12 ? "Cześć" : "Czołem";
             //https://openweathermap.org/current
-            String baseurl = "http://api.openweathermap.org/data/2.5/forecast?q=" + Globalne.city + "," + Globalne.countryCode + "&mode=xml&appid=1f7cff01e11e11b2533f9497da9ee055";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(baseurl);
-            Console.WriteLine(baseurl);
-            request.Method = "POST";
-            request.Accept = "application/json";
-            //request.Credentials = new NetworkCredential(username, password);
-            request.UserAgent = "curl/7.37.0";
-            request.ContentType = "application/x-www-form-urlencoded";
 
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-                string data = "browser=Win7x64-C1|Chrome32|1024x768&url=http://www.google.com";
-
-                streamWriter.Write(data);
-            }
-
-            var response = request.GetResponse();
-            string text;
-
-            using (var sr = new StreamReader(response.GetResponseStream()))
-            {
-                text = sr.ReadToEnd();
-                //Console.WriteLine(text);
-            }            
-            return View("MyView");
+            return View("pogoda");
         }
 
         public IActionResult Privacy()
